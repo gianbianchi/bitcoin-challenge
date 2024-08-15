4e945f8f0cf6443243740f32a8c848477ccf159e
gianbianchi <bianchigian99@gmail.com>
2024-08-15 22:25:23 +0000

    feat: validations - transaction endpoints

--- src/adapters/controllers/finance/sell-bitcoin.controller copy.ts
+++ src/adapters/controllers/finance/sell-bitcoin.controller copy.ts
@@ -11,10 +11,30 @@ export const handleSellBitCoin = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { id, email } = req.user;
-  const { amount } = req.body;
-
   try {
+    const { id, email } = req.user;
+    const { amount } = req.body;
+
+    if (!amount) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Amount is required' });
+    }
+
+    const amountValidation = !isNaN(parseFloat(amount)) && isFinite(amount);
+
+    if (!amountValidation) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Amount must be numeric' });
+    }
+
+    if (amount <= 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Amount must be greater than zero' });
+    }
+
     const response = await useCase.execute(id, amount);
 
     await sendEmail({
--- src/adapters/controllers/transaction/deposit.controller.ts
+++ src/adapters/controllers/transaction/deposit.controller.ts
@@ -15,13 +15,27 @@ export const handleDeposit = async (
   next: NextFunction
 ) => {
   try {
-    const { amount } = req.body;
     const { id, email } = req.user;
+    const { amount } = req.body;
 
     if (!amount) {
       return res
         .status(StatusCodes.BAD_REQUEST)
         .json({ message: 'Amount is required' });
+    }
+
+    const amountValidation = !isNaN(parseFloat(amount)) && isFinite(amount);
+
+    if (!amountValidation) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Amount must be numeric' });
+    }
+
+    if (amount <= 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Amount must be greater than zero' });
     }
 
     const input: CreateTransactionDto = {

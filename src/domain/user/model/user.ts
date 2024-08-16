429623e3511ea3f957a22b195a2ad04ad65b1c66
gianbianchi <bianchigian99@gmail.com>
2024-08-16 09:24:34 -0300

    remove: class validator

--- src/domain/user/model/user.ts
+++ src/domain/user/model/user.ts
@@ -1,23 +1,9 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  IsStrongPassword,
-} from 'class-validator';
 import * as bcrypt from 'bcrypt';
 
 export class User {
   id?: string;
-
-  @IsString()
-  @IsNotEmpty()
   name: string;
-
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
-
-  @IsStrongPassword()
   password: string;
 
   constructor({ name, email, password }: User) {
